3576cde0e62b177619dbaaf308704787dd30695d
zak-sars <zaki.mahdadi20@gmail.com>
2024-10-01 17:10:33 +0100

    formating time for creation

--- src/views/apps/admin/clients/details/info/ClientCreatedBy.tsx
+++ src/views/apps/admin/clients/details/info/ClientCreatedBy.tsx
@@ -8,6 +8,20 @@ type ClientEditProps = {
   clientData: ClientRead | undefined; // Adjust the type as necessary
 };
 
+// Function to format the date
+const formatDate = (dateString: string) => {
+  const date = new Date(dateString);
+
+
+return date.toLocaleString('fr-FR', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+  });
+};
+
 const ClientCreatedBy: React.FC<ClientEditProps> = ({ clientData }) => {
   return (
     <Card>
@@ -25,14 +39,15 @@ const ClientCreatedBy: React.FC<ClientEditProps> = ({ clientData }) => {
 
             <Typography variant="subtitle2" className='mt-4' fontWeight="normal">Plus De Détails</Typography>
             <Divider className='mlb-2' sx={{ height: '1px', width: '50%', marginLeft: '0' }} />
+
             <div className='flex flex-col gap-2'>
               <div className='flex items-center flex-wrap gap-x-1.5'>
                 <Typography className='font-medium' color='text.primary'>Créé à:</Typography>
-                <Typography>{clientData.created_at}</Typography>
+                <Typography>{formatDate(clientData.created_at)}</Typography>
               </div>
               <div className='flex items-center flex-wrap gap-x-1.5'>
                 <Typography className='font-medium' color='text.primary'>Mise à jour à:</Typography>
-                <Typography>{clientData.updated_at}</Typography>
+                <Typography>{formatDate(clientData.updated_at)}</Typography>
               </div>
             </div>
           </>
