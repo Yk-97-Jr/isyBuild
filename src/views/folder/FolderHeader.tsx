7751828531563064f5ead147e8bb90a7aa08ae18
zak-sars <zaki.mahdadi20@gmail.com>
2024-10-18 17:57:54 +0100

    push to login page

--- src/views/folder/FolderHeader.tsx
+++ src/views/folder/FolderHeader.tsx
@@ -2,12 +2,21 @@ import React from 'react';
 
 import Link from "next/link";
 
+import { useRouter } from 'next/navigation';
+
 import Button from '@mui/material/Button';
 
 import Logo from "@components/layout/shared/Logo";
 
 
 const FolderHeader = () => {
+  const router = useRouter();
+
+  const handleLogin = () => {
+    // Navigate to the login page
+    router.push('/login'); // Change '/login' to the path of your login page
+  }
+
   return (
     <div className='flex flex-wrap sm:items-center justify-between max-sm:flex-col gap-6'>
       <div>
@@ -16,7 +25,7 @@ const FolderHeader = () => {
         </Link>
       </div>
       <div className='flex flex-wrap max-sm:flex-col gap-4'>
-        <Button variant='contained'>
+        <Button variant='contained' onClick={handleLogin}>
           Connexion
         </Button>
       </div>
