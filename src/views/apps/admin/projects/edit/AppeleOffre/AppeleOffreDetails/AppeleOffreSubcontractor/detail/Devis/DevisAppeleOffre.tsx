23bfe91872e8330d4d6c303551221163c1bb3965
zak-sars <zaki.mahdadi20@gmail.com>
2024-10-20 15:27:11 +0100

    devis view and delete

---
+++ src/views/apps/admin/projects/edit/AppeleOffre/AppeleOffreDetails/AppeleOffreSubcontractor/detail/Devis/DevisAppeleOffre.tsx
@@ -0,0 +1,168 @@
+'use client';
+
+// MUI Imports
+import React from "react";
+
+import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardContent from '@mui/material/CardContent';
+
+
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import IconButton from '@mui/material/IconButton';
+
+import {Button} from "@mui/material";
+
+import type {ProjectLotSubcontractorRead} from "@/services/IsyBuildApi";
+import OptionMenu from "@core/components/option-menu";
+
+// Type imports for form handling
+type Props = {
+  projectLotSubcontractorData: ProjectLotSubcontractorRead | undefined; // Adjust the type as necessary
+  setOpenAdd: React.Dispatch<React.SetStateAction<boolean>>; // This will be used to control a boolean state
+  setOpenDelete: React.Dispatch<React.SetStateAction<boolean>>; // This will be used to control a boolean state
+  setId: React.Dispatch<React.SetStateAction<number>>; // This will be used to control a boolean state
+
+
+};
+
+const DevisAppeleOffre: React.FC<Props> = ({projectLotSubcontractorData, setOpenAdd, setId, setOpenDelete}) => {
+
+  const handleAdd = () => {
+    setOpenAdd(true)
+  }
+
+  const handleDelete = (id: number) => {
+    setOpenDelete(true)
+    setId(id)
+  }
+
+  const handleHistory = (id: number) => {
+    console.log(id)
+
+  }
+
+  const handleEdit = (id: number) => {
+    console.log(id)
+
+  }
+
+
+  const openOrDownloadFile = (fileUrl: string) => {
+    const fileExtension = fileUrl.split('.').pop()?.toLowerCase();
+
+    // List of file types that can be viewed in the browser
+    const viewableTypes = ['pdf', 'jpg', 'jpeg', 'png', 'txt', 'html', 'gif'];
+
+    if (fileExtension && viewableTypes.includes(fileExtension)) {
+      // Open in new tab if it's a viewable file type
+      window.open(fileUrl, '_blank', 'noopener,noreferrer');
+    } else {
+      // If it's not viewable, force download
+      const link = document.createElement('a');
+
+      link.href = fileUrl;
+      link.setAttribute('download', fileUrl.split('/').pop() || 'file');
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    }
+  };
+
+
+  return (
+    <Card>
+      {projectLotSubcontractorData?.devis_document ? (
+        <div>
+          <CardHeader title='Devis' action={
+            <OptionMenu
+              iconButtonProps={{size: 'medium'}}
+              iconClassName='text-textSecondary'
+              options={[
+                {
+                  text: 'Modifier',
+                  menuItemProps: {
+                    className: 'flex items-center gap-1 text-textSecondary',
+                    onClick: () => handleEdit(projectLotSubcontractorData?.devis_document.id)
+                  }
+                },
+                {
+                  text: 'Historique',
+                  menuItemProps: {
+                    className: 'flex items-center gap-1 text-textSecondary',
+                    onClick: () => handleHistory(projectLotSubcontractorData?.devis_document.id)
+                  }
+                },
+                {
+                  text: 'Supprimer',
+                  menuItemProps: {
+                    className: 'flex items-center gap-1 text-textSecondary',
+                    onClick: () => handleDelete(projectLotSubcontractorData?.devis_document.id)
+                  }
+                }
+              ]}
+            />
+          }/>
+          <CardContent>
+            <Grid container spacing={6} className='mbe-6'>
+              <List sx={{width: '100%', bgcolor: 'background.paper'}}>
+
+                <ListItem
+                  disableGutters
+                  secondaryAction={
+                    <div>
+                      {/* First Icon Button */}
+                      <IconButton
+                        onClick={() => openOrDownloadFile(projectLotSubcontractorData?.devis_document.latest_version.file_url)}>
+                        <i className='tabler-eye text-textSecondary'/>
+                      </IconButton>
+
+                      {/* Second Icon Button */}
+                      <IconButton
+                        onClick={() => openOrDownloadFile(projectLotSubcontractorData?.devis_document.latest_version.file_url)}>
+                        <i className='tabler-download text-textSecondary'/>
+                      </IconButton>
+                    </div>
+                  }
+                >
+                  <ListItemText primary={`${projectLotSubcontractorData?.devis_document?.name ?? 'Devis'}`}/>
+                </ListItem>
+
+              </List>
+            </Grid>
+          </CardContent>
+        </div>
+      ) : (
+        <div>
+          <CardHeader title='Devis'/>
+          <CardContent>
+            <Grid container spacing={6} className='mbe-6'>
+              <List sx={{width: '100%', bgcolor: 'background.paper'}}>
+                <ListItem
+                  disableGutters
+                  secondaryAction={
+                    <div>
+                      <Button
+                        variant="contained"
+                        onClick={handleAdd}
+                      >
+                        Ajouter
+                      </Button>
+                    </div>
+                  }
+                >
+                  <ListItemText primary={`Ajouter un fichier devis`}/>
+                </ListItem>
+
+              </List>
+            </Grid>
+          </CardContent>
+        </div>)}
+    </Card>
+  );
+};
+
+export default DevisAppeleOffre;
