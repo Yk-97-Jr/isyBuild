ac71686f86d824ef646a61f1ab5f684ddc65bca6
Yk-97-Jr <youcefkhalfi5@gmail.com>
2024-11-18 09:57:30 +0100

    add dashbord pages

---
+++ src/app/(dashboard)/subcontractor/projects/list/page.tsx
@@ -0,0 +1,11 @@
+// React Imports
+import React from 'react'
+
+
+const ProjectsListApp = () => {
+
+
+  return <div>this lis page for sub <h1>Project</h1></div>
+}
+
+export default ProjectsListApp
